ae226084c5f50feeae4aae23dcd4fd6e7d697440
programacaogitthais <202501135641@alunos.estacio.br>
2025-05-21 00:13:36 +0000

    testes

---
+++ Teste4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() { //ABRE A CHAVE
+  int numero1 = 10;
+  int numero2 = 5;
+
+  if (numero1 > numero2) {
+    printf("numero1 é maior que numero2\n");
+  }  //FECHA A CHAVE
+
+  return 0;
+}
